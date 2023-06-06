0f53767685b5ab87a3e2c761e666bcc34fae376e
Agustin Andres Gutierrez <agutierrez@itba.edu.ar>
2023-06-06 16:03:25 -0300

    Light device info file

--- app/src/main/java/com/example/intelicasamobile/MainActivity.kt
+++ app/src/main/java/com/example/intelicasamobile/MainActivity.kt
@@ -8,16 +8,22 @@ import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -25,19 +31,29 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.intelicasamobile.ui.theme.IntelicasaMobileTheme
+import com.github.skydoves.colorpicker.compose.ColorEnvelope
+import com.github.skydoves.colorpicker.compose.HsvColorPicker
+import com.github.skydoves.colorpicker.compose.rememberColorPickerController
 
 
 class MainActivity : ComponentActivity() {
@@ -58,7 +74,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
-@Preview(showBackground = true)
+@Preview(showBackground = true, name = "Intelicasa")
 @Composable
 fun IntelicasaApp() {
     Scaffold(
@@ -143,29 +159,6 @@ fun DeviceCard(
     }
 }
 
-//@Preview(name = "DeviceInfo")
-@Composable
-fun DeviceInfoPreview() {
-    DeviceInfo(device = devices[0])
-}
-
-@Composable
-fun DeviceInfo(
-    device: Device,
-    modifier: Modifier = Modifier
-) {
-    Card(modifier = modifier) {
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .padding(dimensionResource(id = R.dimen.padding_small))) {
-            Image(painter = painterResource(id = device.deviceType.imageResourceId), contentDescription = "", modifier= Modifier.size(dimensionResource(id = R.dimen.image_size)))
-
-            Text(text = stringResource(id = device.name), modifier= Modifier.padding(start = dimensionResource(
-                id = R.dimen.padding_small))
-            )
-        }
-    }
-}
 
 data class Device(
     val deviceType: DeviceTypes,
