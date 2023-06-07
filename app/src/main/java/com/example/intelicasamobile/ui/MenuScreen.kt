d1d6bdaa7c21f24eb8c230dcda9269ca0944f4d9
JeremiasFeferovich <95632444+JeremiasFeferovich@users.noreply.github.com>
2023-06-07 11:44:19 -0300

    implemented navigation

---
+++ app/src/main/java/com/example/intelicasamobile/ui/MenuScreen.kt
@@ -0,0 +1,19 @@
+package com.example.intelicasamobile.ui
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun MenuScreen() {
+    Surface(
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Text(text = "Menu")
+    }
+}
