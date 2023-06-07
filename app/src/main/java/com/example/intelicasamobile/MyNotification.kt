bb86ba1689073625a37052b46cf1c5538752c1e1
JeremiasFeferovich <95632444+JeremiasFeferovich@users.noreply.github.com>
2023-06-07 14:46:12 -0300

    implemented notifications

---
+++ app/src/main/java/com/example/intelicasamobile/MyNotification.kt
@@ -0,0 +1,37 @@
+package com.example.intelicasamobile
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+
+class MyNotification(
+    var context: Context, var title: String, var message: String){
+    val channelID: String = "com.example.intelicasamobile"
+    val channelName: String = "Intelicasa"
+    val notificationManager=context.applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    lateinit var notificationChannel: NotificationChannel
+    lateinit var notificationBuilder: NotificationCompat.Builder
+
+    fun fireNotification(){
+        notificationChannel = NotificationChannel(channelID, channelName, NotificationManager.IMPORTANCE_HIGH).apply {
+            enableLights(true)
+            enableVibration(true)
+        }
+        notificationManager.createNotificationChannel(notificationChannel)
+
+        val intent = Intent(context, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+        notificationBuilder = NotificationCompat.Builder(context, channelID).apply {
+            setSmallIcon(R.drawable.ic_launcher_foreground)
+            setContentTitle(title)
+            setContentText(message)
+            setAutoCancel(true)
+            addAction(R.drawable.ic_launcher_foreground, "Open Message", pendingIntent)
+        }
+        notificationManager.notify(100, notificationBuilder.build())
+
+    }
+}
--- app/src/main/java/com/example/intelicasamobile/ui/MenuScreen.kt
+++ app/src/main/java/com/example/intelicasamobile/ui/MenuScreen.kt
@@ -1,11 +1,20 @@
 package com.example.intelicasamobile.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.intelicasamobile.MyNotification
+import com.example.intelicasamobile.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true)
@@ -14,6 +23,25 @@ fun MenuScreen() {
     Surface(
         color = MaterialTheme.colorScheme.background
     ) {
-        Text(text = "Menu")
+        Column(
+            modifier= Modifier
+                .fillMaxSize()
+                .padding(dimensionResource(id = R.dimen.padding_medium))
+        ) {
+            Text(text = "Menu", style = MaterialTheme.typography.bodyLarge)
+            NotificationButton()
+        }
+
+    }
+}
+
+@Composable
+fun NotificationButton() {
+    val context = LocalContext.current
+    Button(onClick = {
+        val notification = MyNotification(context=context, title="Test Title", message = "Test Body")
+        notification.fireNotification()
+    }) {
+        Text(text = "Send Notification")
     }
 }
