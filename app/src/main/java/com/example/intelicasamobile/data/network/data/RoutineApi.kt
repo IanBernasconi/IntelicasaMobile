42de7e1f388613154bf86d847ade612871dc8168
Agustin Andres Gutierrez <agutierrez@itba.edu.ar>
2023-06-11 16:21:24 -0300

    Routine Api

---
+++ app/src/main/java/com/example/intelicasamobile/data/network/data/RoutineApi.kt
@@ -0,0 +1,45 @@
+package com.example.intelicasamobile.data.network.data
+
+import Api
+import com.example.intelicasamobile.model.Action
+import com.example.intelicasamobile.model.ActionTypes
+import com.example.intelicasamobile.model.Routine
+import org.json.JSONObject
+
+object RoutineApi {
+
+    private fun getUrl(slug: String? = null): String {
+        return "${Api.BASE_URL}/routines${slug?.let { "/$it" } ?: ""}"
+    }
+
+    suspend fun getAll(): List<Routine> {
+        val routinesJson = Api.get(getUrl())?.getJSONArray("result")
+        return routinesJson?.let { array ->
+            (0 until array.length()).map { index ->
+                val routine = array.getJSONObject(index)
+                Routine(
+                    id = routine.getString("id"),
+                    name = routine.getString("name"),
+                    actions = routine.getJSONArray("actions").let { actions ->
+                        (0 until actions.length()).map { actionIndex ->
+                            val action = actions.getJSONObject(actionIndex)
+                            Action(
+                                action = ActionTypes.values().find{ it.apiName == action.getString("action") } ?: ActionTypes.TURN_ON,
+                                deviceId = action.getJSONObject("device").getString("id"),
+                                params = listOf(action.getString("value"))
+                            )
+                        }
+                    }
+                )
+            }
+        } ?: emptyList()
+    }
+
+    suspend fun execute(routine: Routine): JSONObject? {
+        return Api.put("${getUrl(routine.id)}/execute", JSONObject(routine.id))
+    }
+
+    suspend fun get(id: String): JSONObject? {
+        return Api.get(getUrl(id))
+    }
+}
